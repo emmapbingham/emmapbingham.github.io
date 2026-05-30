8a5ea9ee72e296e8427d4efb0a68bba5c40ffbd1
Emma Bingham <13723615+emmapbingham@users.noreply.github.com>
2026-05-30 11:50:33 -0400

    Initial commit of website

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+module.exports = function(eleventyConfig) {
+  // Copy CSS and assets
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/img");
+
+  eleventyConfig.addFilter("year", (date) => new Date(date).getFullYear());
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      layouts: "_includes/layouts",
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+  };
+};
